ac400a579f0f1d17d98cb9875920b2f71707fc25
maryamboneh <maryamboneh@gmail.com>
2021-11-06 21:22:06 +0330

    hand detector

--- hands.py
+++ hands.py
@@ -1,6 +1,9 @@
 import cv2
 import mediapipe as mp
 
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
@@ -23,14 +26,22 @@ with mp_hands.Hands(
     image.flags.writeable = False
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     results = hands.process(image)
-    print("right or left?? ", results.multi_handedness) # for left an right
+    # print("right or left?? ", results.multi_handedness) # for left an right
+    fingers = []
 
     # Draw the hand annotations on the image.
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    print(results.multi_hand_landmarks)
+    print('________________________________________')
     if results.multi_hand_landmarks:
-      for hand_landmarks in results.multi_hand_landmarks:
-        # print(hand_landmarks) # hand position
+      for i, hand_landmarks in enumerate(results.multi_hand_landmarks):
+        fingers.append(hand_landmarks)
+        print('+++++++++++++', fingers)
+        for c in range(len(fingers)):
+          print(c)
+          # print(f'{c} ======= ', fingers[c]) # hand position
+          # print('++++++++++++++++++++++++')
         mp_drawing.draw_landmarks(
             image,
             hand_landmarks,
