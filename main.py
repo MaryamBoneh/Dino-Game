3d1f9ccd6b128c3c70c38dc210a917524365a387
maryamboneh <maryamboneh@gmail.com>
2021-11-10 16:37:32 +0330

    merg 2 codes

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import arcade, time, random
+import arcade, random
+import cv2 
+import mediapipe as mp
+from math import hypot
+import numpy as np 
 
 
 class Game(arcade.Window):
@@ -11,6 +15,10 @@ class Game(arcade.Window):
         arcade.set_background_color(arcade.color.WHITE)
         self.dino = Dino()
         self.grounds = arcade.SpriteList()
+        self.cap = cv2.VideoCapture(0)
+        self.mpHands = mp.solutions.hands 
+        self.hands = self.mpHands.Hands()
+        self.mpDraw = mp.solutions.drawing_utils
 
         for i in range(0, self.w + 132, 132):
             ground = Ground(i, 50)
@@ -18,12 +26,14 @@ class Game(arcade.Window):
 
         self.physics_engine = arcade.PhysicsEnginePlatformer(self.dino, self.grounds, self.gravity)
 
+
     def on_draw(self):
         arcade.start_render()
         for ground in self.grounds:
             ground.draw()
 
         self.dino.draw()
+   
 
     def on_update(self, delta_time: float):
         self.physics_engine.update()
@@ -33,22 +43,41 @@ class Game(arcade.Window):
         self.msec += 0.5
         self.dino.show_walking(self.msec)
 
+        success,img = self.cap.read()
+        imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        results = self.hands.process(imgRGB)
+
+        lmList = []
+        if results.multi_hand_landmarks:
+            for handlandmark in results.multi_hand_landmarks:
+                for id,lm in enumerate(handlandmark.landmark):
+                    h,w,_ = img.shape
+                    cx,cy = int(lm.x*w),int(lm.y*h)
+                    lmList.append([id,cx,cy]) 
+                self.mpDraw.draw_landmarks(img,handlandmark,self.mpHands.HAND_CONNECTIONS)
+
+        if lmList != []:
+            x1,y1 = lmList[4][1], lmList[4][2]
+            x2,y2 = lmList[8][1], lmList[8][2]
+            x3,y3 = lmList[12][1], lmList[12][2]
+            x4,y4 = lmList[16][1], lmList[16][2]
+            x5,y5 = lmList[20][1], lmList[20][2]
+
+            length54 = hypot(x5-x4 , y5-y4)
+            length43 = hypot(x4-x3 , y4-y3)
+            length32 = hypot(x3-x2 , y3-y2)
+            length21 = hypot(x2-x1 , y2-y1)
+
+            if length21 < 50 and length32 < 50 and length43 < 50 and length54 < 50:
+                if self.physics_engine.can_jump():
+                    self.dino.change_y = 15
+            
+        cv2.imshow('Image',img)
+
         for ground in self.grounds:
             if ground.center_x < 0:
                 self.grounds.remove(ground)
                 self.grounds.append(Ground(self.w + 132 ,50))
-
-    def on_key_press(self, key, modifiers):
-        if key == arcade.key.UP:
-            if self.physics_engine.can_jump():
-                self.dino.change_y = 15
-
-        elif key == arcade.key.DOWN:
-            self.dino.bent = True
-
-    def on_key_release(self, key, modifiers):
-        if key==arcade.key.DOWN:
-            self.dino.bent = False
 
 
 class Ground(arcade.Sprite):
